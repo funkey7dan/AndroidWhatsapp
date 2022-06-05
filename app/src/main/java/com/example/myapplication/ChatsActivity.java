ad95206321e7cb599c455f2bdad4db3065c04126
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2022-06-05 09:28:51 +0300

    added spinner animation and state

--- app/src/main/java/com/example/myapplication/ChatsActivity.java
+++ app/src/main/java/com/example/myapplication/ChatsActivity.java
@@ -1,23 +1,27 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 
 public class ChatsActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
-
+        ProgressBar loadSpinner = findViewById(R.id.progressBar);
         ImageView imageView = findViewById(R.id.chatContactImage);
         imageView.setClipToOutline(true);
 
@@ -26,12 +30,29 @@ public class ChatsActivity extends AppCompatActivity {
         for (int i=0;i<10;i++) {
             messagesList.add(new MessageModel("Hi", i % 2 == 0 ? MessagesAdapter.MESSAGE_TYPE_IN : MessagesAdapter.MESSAGE_TYPE_OUT));
         }
-
+        // observe the loading state and hide the loading spinner if loading is complete
+        isLoading.observe(this, state -> {
+            if (state){
+                loadSpinner.setVisibility(View.VISIBLE);
+            }
+            else{
+                loadSpinner.setVisibility(View.INVISIBLE);
+            }
+        });
         MessagesAdapter adapter = new MessagesAdapter(this, messagesList);
 
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+    }
+
+    /**
+     *  load contacts to local data base form server and refresh view
+     */
+    public void loadMessages(){
+        isLoading.setValue(true);
+        // TODO: add message fetching via api
 
     }
 }
--- app/src/main/java/com/example/myapplication/ContactsListActivity.java
+++ app/src/main/java/com/example/myapplication/ContactsListActivity.java
@@ -1,12 +1,15 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,6 +19,7 @@ import java.util.List;
 public class ContactsListActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
 
     String s1[] = {"Alice", "Bob", "Charlie", "Dan", "Eve", "Freddie", "Giovanni", "Herbert"};
     String s2[] = {"I'm alice", "I'm Bob", "I'm Charlie", "I'm Dan", "I'm Eve", "I'm Freddie", "I'm Giovanni", "I'm Herbert"};
@@ -27,19 +31,28 @@ public class ContactsListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contacts_list);
 
         recyclerView = findViewById(R.id.contactsList);
-
+        ProgressBar loadSpinner = findViewById(R.id.progressBar);
         MyAdapter myAdapter = new MyAdapter(this, s1, s2, s3);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         ImageView myImage = findViewById(R.id.myImage);
         myImage.setClipToOutline(true);
-
+        // observe the loading state and hide the loading spinner if loading is complete
+        isLoading.observe(this, state -> {
+            if (state){
+                loadSpinner.setVisibility(View.VISIBLE);
+            }
+            else{
+                loadSpinner.setVisibility(View.INVISIBLE);
+            }
+        });
         FloatingActionButton fab = findViewById(R.id.floatingAddContact);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDialog();
+                //openDialog(); // TODO return button functionality
+                isLoading.setValue(Boolean.FALSE.equals(isLoading.getValue())); //tester to toggle loading state
 
 //                Toast toast = Toast.makeText(getApplicationContext(),
 //                        "Add Contact",
@@ -48,6 +61,15 @@ public class ContactsListActivity extends AppCompatActivity {
 //                toast.show();
             }
         });
+    }
+
+    /**
+     *  load contacts to local data base form server and refresh view
+     */
+    public void loadContacts(){
+        isLoading.setValue(true);
+        // TODO: add contact fetching via api
+
     }
 
     public void openDialog() {
